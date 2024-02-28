d3a91ff7f1a9811d022ff447374bcb9e4d9e9c78
Bee <huuphuoc252513@gmail.com>
2024-02-28 21:15:24 +1100

    update script

---
+++ script.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS user_profile (
+    id BIGSERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOt NULL,
+    email TEXT UNIQUE NOT NULL,
+    gender TEXT CHECK(gender IN ('MALE', 'FEMALE')) NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS youtube_account (
+    id BIGINT PRIMARY KEY REFERENCES user_profile(id),
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS youtube_channel (
+    id BIGSERIAL PRIMARY KEY,
+    youtube_account_id BIGINT NOT NULL REFERENCES youtube_account(id),
+    channel_name TEXT NOT NULL UNIQUE,
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS channel_subscriber (
+    youtube_account_id BIGINT REFERENCES youtube_account(id),
+    youtube_channel BIGINT REFERENCES youtube_channel(id),
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    PRIMARY KEY (youtube_account_id, youtube_channel_id) -- call Composite key
+);
